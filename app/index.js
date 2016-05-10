e4919cb1c47a3f99c74c1c7220ee12ebbcc9663e
Tyson Cleary <amec@tyscorp.net>
2016-05-09 22:30:53 -0500

    react-hot-loader 3

--- app/index.js
+++ app/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import { AppContainer } from 'react-hot-loader';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, hashHistory } from 'react-router';
+import { hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import routes from './routes';
 import configureStore from './store/configureStore';
-import './app.global.css';
+import App from './app';
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
 
+const rootElement = document.getElementById('root');
+
 render(
-  <Provider store={store}>
-    <Router history={history} routes={routes} />
-  </Provider>,
-  document.getElementById('root')
+  <AppContainer>
+    <App store={store} history={history} />
+  </AppContainer>,
+  rootElement
 );
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app');
+
+    render(
+      <AppContainer>
+        <NextApp store={store} history={history} />
+      </AppContainer>,
+      rootElement
+    );
+  });
+}
